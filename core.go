f313063150e52f1f71f85a2771f4f4eee71c7b65
Kurt Hakim <kurthakim@gmail.com>
2022-12-08 15:50:26 +0700

    Added ch1

---
+++ core.go
@@ -0,0 +1,25 @@
+package main
+
+import "errors"
+
+var store = make(map[string]string)
+
+var ErrorNoSuchKey = errors.New("no such key")
+
+func Get(key string) (string, error) {
+	value, ok := store[key]
+	if !ok {
+		return "", ErrorNoSuchKey
+	}
+	return value, nil
+}
+
+func Delete(key string) error {
+	delete(store, key)
+	return nil
+}
+
+func Put(key string, value string) error {
+	store[key] = value
+	return nil
+}
